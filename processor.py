c7cea765bcdc40068e6d654c8118719bd77f82ca
Debangana Choudhury <debangana.chou@gmail.com>
2026-03-13 07:43:52 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,11 +1,8 @@
 import time
-
 logs=[]
 
 def process(log):
-
     try:
-
         client,timestamp,level,msg=log.split("|")
 
         logs.append((float(timestamp),client,level,msg))
@@ -14,10 +11,8 @@ def process(log):
 
         latest=logs[-1]
 
-        print(
-            f"[{time.strftime('%H:%M:%S',time.localtime(latest[0]))}] "
+        print(f"[{time.strftime('%H:%M:%S',time.localtime(latest[0]))}] "
             f"{latest[1]} {latest[2]} {latest[3]}"
         )
-
     except:
         pass
