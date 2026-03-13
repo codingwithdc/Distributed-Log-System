e976e7701f852bfdf3e2d936473aa7d59af5922f
Debangana Choudhury <debangana.chou@gmail.com>
2026-03-13 07:03:45 +0530

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -2,69 +2,51 @@ import socket
 import threading
 import queue
 import time
-
 from processor import process
 from metrics import Metrics
 
-HOST="0.0.0.0"
-PORT=9000
-BUFFER=1024
-QUEUE_SIZE=5000
+HOST = "0.0.0.0"
+PORT = 9000
+BUFFER = 1024
+QUEUE_SIZE = 5000
 
-log_queue=queue.Queue(maxsize=QUEUE_SIZE)
-metrics=Metrics()
-
+log_queue = queue.Queue(maxsize=QUEUE_SIZE)
+metrics = Metrics()
 
 def worker():
-
     while True:
-
-        log=log_queue.get()
-
+        log = log_queue.get()
         process(log)
-
         log_queue.task_done()
 
-
 def monitor():
-
     while True:
-
         time.sleep(5)
-
         metrics.report()
 
-
 def start():
-
-    s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-    s.bind((HOST,PORT))
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, PORT))
 
     print("UDP log server running")
 
     while True:
-
-        data,addr=s.recvfrom(BUFFER)
-
-        log=data.decode()
+        data, addr = s.recvfrom(BUFFER)
+        log = data.decode()
 
         try:
-
-            log_queue.put(log,block=False)
-
+            log_queue.put(log, block=False)
             metrics.received()
 
         except queue.Full:
-
             metrics.drop()
 
+# Start worker threads
+for i in range(4):
+    threading.Thread(target=worker, daemon=True).start()
 
-if __name__=="__main__":
+# Start metrics monitor
+threading.Thread(target=monitor, daemon=True).start()
 
-    for i in range(4):
-        threading.Thread(target=worker,daemon=True).start()
-
-    threading.Thread(target=monitor,daemon=True).start()
-
-    start()
+# Start server
+start()
