73057729cba679832404334bdeb5415fa8f2d239
Debangana Choudhury <debangana.chou@gmail.com>
2026-04-16 09:02:07 +0530

    updated generate_cert.sh

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,1 +1,3 @@
-openssl req -new -x509 -days 365 -nodes -out server.crt -keyout server.key
+#!/bin/bash
+openssl req -new -x509 -days 365 -nodes -out server.crt -keyout server.key -subj "/CN=127.0.0.1"
+
